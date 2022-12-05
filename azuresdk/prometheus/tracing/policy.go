7109770a084a1c7cd3b939adc9354619b0ea9565
Markus Blaschke <mblaschke82@gmail.com>
2022-12-05 23:54:17 +0100

    add costmanagement ratelimit
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- azuresdk/prometheus/tracing/policy.go
+++ azuresdk/prometheus/tracing/policy.go
@@ -1,6 +1,7 @@
 package tracing
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -97,8 +98,10 @@ func (p tracingPolicy) Do(req *policy.Request) (*http.Response, error) {
 
 	if prometheusAzureApiRatelimit != nil {
 		collectAzureApiRateLimitMetric := func(r *http.Response, headerName string, scopeLabel, typeLabel string) {
-			ratelimit := r.Header.Get(headerName)
-			if v, err := strconv.ParseInt(ratelimit, 10, 64); err == nil {
+			headerValue := r.Header.Get(headerName)
+
+			if v, err := strconv.ParseInt(headerValue, 10, 64); err == nil {
+				// single value
 				prometheusAzureApiRatelimit.With(prometheus.Labels{
 					"apiEndpoint":    hostname,
 					"subscriptionID": subscriptionId,
@@ -106,6 +109,25 @@ func (p tracingPolicy) Do(req *policy.Request) (*http.Response, error) {
 					"scope":          scopeLabel,
 					"type":           typeLabel,
 				}).Set(float64(v))
+			} else if strings.Contains(headerValue, ":") {
+
+				// multi value (comma sparated eg "QueriesPerHour:496,QueriesPerMin:37,QueriesPer10Sec:11")
+				for _, val := range strings.Split(headerValue, ",") {
+					if parts := strings.SplitN(val, ":", 2); len(parts) == 2 {
+						quotaName := parts[0]
+						quotaValue := parts[1]
+						if v, err := strconv.ParseInt(quotaValue, 10, 64); err == nil {
+							prometheusAzureApiRatelimit.With(prometheus.Labels{
+								"apiEndpoint":    hostname,
+								"subscriptionID": subscriptionId,
+								"tenantID":       tenantId,
+								"scope":          scopeLabel,
+								"type":           fmt.Sprintf("%s.%s", typeLabel, quotaName),
+							}).Set(float64(v))
+						}
+					}
+				}
+
 			}
 		}
 
@@ -114,10 +136,16 @@ func (p tracingPolicy) Do(req *policy.Request) (*http.Response, error) {
 			collectAzureApiRateLimitMetric(res, "x-ms-user-quota-remaining", "resourcegraph", "quota")
 		}
 
+		// costmanagement limits
+		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-microsoft.costmanagement-qpu-consumed", "costmanagement", "qpu-consumed")
+		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-microsoft.costmanagement-qpu-remaining", "costmanagement", "qpu-remaining")
+		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-remaining-microsoft.costmanagement-entity-requests", "costmanagement", "entity-requests")
+		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-remaining-microsoft.costmanagement-tenant-requests", "costmanagement", "tenant-requests")
+
 		// subscription rate limits
 		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-remaining-subscription-reads", "subscription", "reads")
 		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-remaining-subscription-writes", "subscription", "writes")
-		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-remaining-subscription-resource-requests", "subscription", "resource-requests")
+		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-remaining-subscription-resource-requests", "subscription", "resourceRequests")
 		collectAzureApiRateLimitMetric(res, "x-ms-ratelimit-remaining-subscription-resource-entities-read", "subscription", "resource-entities-read")
 
 		// tenant rate limits

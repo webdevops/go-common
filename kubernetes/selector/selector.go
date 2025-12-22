123cc2aab6b23ea8883aab6cc058c9fc73faff47
Markus Blaschke <mblaschke82@gmail.com>
2025-12-22 13:21:08 +0100

    add kubernetes
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

---
+++ kubernetes/selector/selector.go
@@ -0,0 +1,51 @@
+package selector
+
+import (
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	SelectorError = "<error>"
+	SelectorNone  = "<none>"
+)
+
+type (
+	KubernetesSelector struct {
+		metav1.LabelSelector `json:",inline"`
+		selector             *string
+	}
+)
+
+// IsEmpty checks if the selector is empty/defined or not
+func (selector *KubernetesSelector) IsEmpty() bool {
+	if selector == nil || (len(selector.MatchLabels) == 0 && len(selector.MatchExpressions) == 0) {
+		return true
+	}
+
+	return false
+}
+
+// Compile compiles the label selector struct to a string
+func (selector *KubernetesSelector) Compile() (string, error) {
+	// no selector
+	if selector.IsEmpty() {
+		return "", nil
+	}
+
+	if selector.selector == nil {
+		labelSelector := metav1.FormatLabelSelector(&selector.LabelSelector)
+
+		switch labelSelector {
+		case SelectorError:
+			return "", fmt.Errorf(`unable to compile Kubernetes selector for resource: %v`, selector)
+		case SelectorNone:
+			labelSelector = ""
+		}
+
+		selector.selector = &labelSelector
+	}
+
+	return *selector.selector, nil
+}
